0ec182ae4cffa8f8e00a56be87aa6f8b51bf81c2
chihaicheng <chc2075983104@163.com>
2025-03-24 21:59:38 +0800

    feat: 我的页面开发中

--- app/src/main/java/com/chc/roundmeeting/ui/page/home/my/MyPage.kt
+++ app/src/main/java/com/chc/roundmeeting/ui/page/home/my/MyPage.kt
@@ -1,10 +1,43 @@
 package com.chc.roundmeeting.ui.page.home.my
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyPage(modifier: Modifier = Modifier) {
-    Text("我的主页")
+    Surface(
+        modifier = modifier
+            .statusBarsPadding()
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            TopAppBar(
+                title = {},
+                actions = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(Icons.Rounded.Settings, contentDescription = null)
+                    }
+                }
+            )
+        }
+    }
 }
